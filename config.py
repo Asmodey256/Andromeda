ecb56d863c87754ce125e461a7d0bcf818314946
Asmodey256 <97547000+Asmodey256@users.noreply.github.com>
2022-02-24 00:02:16 +0300

    ver1 Banbot

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "5213235557:AAEgP2XIQqX4Oxfol0RTODyqwV3hECv5kWE"
+
+keys = {
+    "доллар": "USD",
+    "евро": "EUR",
+    "рубль": "RUB",
+}
---
+++ extensions.py
@@ -0,0 +1,40 @@
+import requests
+import json
+from config import keys
+
+class ConvertException(Exception):
+    pass
+
+class ValutConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+
+        if quote == base:
+            raise ConvertException(f"Невозможно перевести одинаковые валюты {base}")
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertException(f"Не удалось обработать валюту {quote}")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertException(f"Не удалось обработать валюту {base}")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertException(f"Не удалось обработать колличество {amount}")
+
+        quote_ticker = keys[quote]
+        base_ticker = keys[base]
+
+        r = requests.get(f"https://free.currconv.com/api/v7/convert?q={quote_ticker}_{base_ticker}&compact=ultra&apiKey=e9c184ee7785b7ec0ca7")
+        total = json.loads(r.content)
+        list_total = str(list(total.values()))
+        list_total = list_total.replace("[", "")
+        list_total = list_total.replace("]", "")
+        total_base = float(list_total) * float(amount)
+
+        return total_base
